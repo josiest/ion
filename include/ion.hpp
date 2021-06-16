54c6b2fd6c75a790e08da4c3db59055f753ab0ca
Josie Thompson <dot.slash.josie@gmail.com>
2021-06-16 10:54:56 -0700

    changed structure to be one source and header file

--- include/ion.hpp
+++ include/ion.hpp
@@ -1,6 +1,44 @@
 #ifndef ION_HPP
 #define ION_HPP
 
-#include "app.hpp"
+#include <SDL2/SDL.h>
+#include <vector>
+#include <string>
+
+namespace ion {
+
+typedef void (*Listener)();
+
+/**
+ * An SDL resource/event handler
+ */
+class app {
+    SDL_Window * _window;
+    SDL_Renderer * _renderer;
+    std::vector<Listener> _update_listeners;
+public:
+    /**
+     * Create an app.
+     *
+     * :param title: the title of the program.
+     * :param width: the width of the window.
+     * :param height: the height of the window.
+     */
+    app(const std::string & title, int width, int height);
+    ~app();
+
+    /** The game's window */
+    inline SDL_Window * window() { return _window; }
+    /** The game's renderer */
+    inline SDL_Renderer * renderer() { return _renderer; }
+
+    /** Connect a function to be called on update */
+    inline void connect_update_listener(Listener callback)
+    { _update_listeners.push_back(callback); }
+
+    /** The main loop of this game */
+    void run();
+};
+}
 
 #endif
--- src/ion.cpp
+++ src/ion.cpp
@@ -1,4 +1,4 @@
-#include "app.hpp"
+#include "ion.hpp"
 
 #include <SDL2/SDL.h>
 
