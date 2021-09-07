68240247fbb64444f31551846a7994c10cd4c8dc
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-06 19:41:45 -0700

    muncher now creates window

--- examples/muncher/src/muncher.cpp
+++ examples/muncher/src/muncher.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
+#include "ion.hpp"
+#include <SDL2/SDL.h>
+
+void render(ion::Window & window)
+{
+    auto renderer = window.sdl_renderer();
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+// resource interface for quitting the program
+bool HAS_QUIT = false;
+bool has_quit() { return HAS_QUIT; }
+void quit(SDL_Event const &) { HAS_QUIT = true; }
+
+void muncher()
+{
+    // create the sdl-event handler
+    ion::EventHandler handler;
+    handler.subscribe(SDL_QUIT, &quit);
+
+    // create the window, specifying title and dimensions
+    auto window = ion::unique_basic_window("Muncher", 800, 600);
+
+    // run the program
+    while (!has_quit()) {
+
+        handler.process_queue();
+        render(*window);
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    // run the muncher game then quit sdl
+    muncher();
+    SDL_Quit();
 }
+
