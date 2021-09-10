1be6834d80f26e4df9c7c212136a83de19ce999d
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-10 12:30:36 -0700

    added reset functionality

--- examples/muncher/src/muncher.cpp
+++ examples/muncher/src/muncher.cpp
@@ -20,18 +20,23 @@ bool has_quit() { return HAS_QUIT; }
 // an event-callback to set the quit-flag to true
 void quit(SDL_Event const &) { HAS_QUIT = true; }
 
+void reset_game(SDL_Event const & event);
+
 // a class that manages sdl resources
 class Muncher {
 public:
     using engine_t = std::mt19937;
     // intialize the muncher game object with the specified width and height
     Muncher(size_t width, size_t height)
-        : _title{"Muncher"}, _width(width), _height(height), _handler(),
-          _input(_handler, SDLK_d, SDLK_a, SDLK_w, SDLK_s), _registry(),
-          _rng{std::random_device{}()}, _munchables{_width, _height, _rng}
+        : _title{"Muncher"}, _width(width), _height(height),
+          _handler(), _input(_handler, SDLK_d, SDLK_a, SDLK_w, SDLK_s),
+          _rng{std::random_device{}()}, 
+          _registry(),
+          _player(_make_player()), _munchables{_width, _height, _rng}
     {
         // the choice to subscribe the event listeners here is somewhat arbitrary
         _handler.subscribe(SDL_QUIT, &quit);
+        _handler.subscribe(SDL_KEYUP, &reset_game);
     }
 
     // make sure that sdl quits once the game goes out of scope
@@ -41,8 +46,6 @@ public:
     {
         // create the window, specifying title and dimensions
         auto window = ion::basic_window(_title, _width, _height);
-        auto player = make_player(_registry, static_cast<int>(_width)/2,
-                                             static_cast<int>(_height)/2, 15);
 
         // timer for physics
         uint32_t prev_time = SDL_GetTicks();
@@ -64,14 +67,27 @@ public:
             if (munch_time(_rng)) {
                 _munchables.make_munchable(_registry);
             }
-            accelerate_player(_registry, player, _input, dt);
+            accelerate_player(_registry, _player, _input, dt);
             move_munchies(_registry, dt);
-            devour(_registry, player);
+            devour(_registry, _player);
             _munchables.filter(_registry);
 
             // render
             render(window, _registry);
         }
+    }
+
+    entt::entity _make_player()
+    {
+        return make_player(_registry, static_cast<int>(_width)/2,
+                                      static_cast<int>(_height)/2, 15);
+    }
+    void reset()
+    {
+        if (_registry.valid(_player)) {
+            _registry.destroy(_player);
+        }
+        _player = _make_player();
     }
 private:
     std::string const _title;
@@ -89,18 +105,28 @@ private:
     // This _could_ be achieved by explicitly calling `window.reset()` before
     // `SDL_Quit()`, but having this done implicitly via scoping rules is
     // arguably more elegant
-
     ion::input::KeyboardAxis2D _input;
-    entt::registry _registry;
 
     engine_t _rng;
+    entt::registry _registry;
+
+    entt::entity _player;
     MunchableFactory _munchables;
 };
+
+Muncher GAME{800, 600};
+void reset_game(SDL_Event const & event)
+{
+    // do nothing if not the right event or not the right key
+    if (event.type != SDL_KEYUP || event.key.keysym.sym != SDLK_r) {
+        return;
+    }
+    GAME.reset();
+}
 
 int main()
 {
     // run the muncher game then quit sdl
-    Muncher game{800, 600};
-    game.run();
+    GAME.run();
 }
 
