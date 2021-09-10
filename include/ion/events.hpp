5a4f1b39a51dc7de06d3365e575501ed67d25f96
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-10 13:47:35 -0700

    added documentation for event listener

--- include/ion/events.hpp
+++ include/ion/events.hpp
@@ -14,8 +14,31 @@ class EventHandler {
 public:
     inline EventHandler() {}
 
+    /**
+     * Subscribe a function pointer listener
+     *
+     * \param event_type the SDL event type to listen for
+     * \param listener the function to call back to when event_type is triggered
+     *
+     * \note This should be the preferred method for subscribing functions
+     *       because it has the least overhead
+     */
     void subscribe(Uint32 event_type, ListenerFP listener);
+
+    /**
+     * Subscribe an arbitrary function listener
+     *
+     * \param event_type the SDL event type to listen for
+     * \param listener the function to call back to when event_type is triggered
+     *
+     * \note Be cautious when using this method because std::function has a lot
+     *       of overhead and it may unnecesarily slow down your game
+     */
     void subscribe_functor(Uint32 event_type, ListenerFXN listener);
+
+    /**
+     * Poll all SDL events in the queue, calling any listeners along the way.
+     */
     void process_queue();
 private:
     std::unordered_map<Uint32, std::vector<ListenerFP>> _listeners_by_type;
