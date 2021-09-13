881aeee275b726b1fde309057032e57e8dd147f9
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-12 23:21:49 -0700

    made some changes to window implementation

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include <sstream>
 
 namespace ion {
 
@@ -24,11 +25,15 @@ window::~window()
     }
 }
 
-window basic_window(std::string const & title, size_t width, size_t height)
+// helper functions for creating windows
+SDL_Window * load_window(std::string const & title, size_t width, size_t height)
 {
     // initialize video if it hasn't already been done
     if (!SDL_WasInit(SDL_INIT_VIDEO) && SDL_InitSubSystem(SDL_INIT_VIDEO)) {
-        throw std::runtime_error{SDL_GetError()};
+        std::stringstream message;
+        message << "SDL couldn't initialize video! SDL_Error: "
+                << SDL_GetError();
+        throw std::runtime_error{message.str()};
     }
 
     // create the window and a renderer for it
@@ -40,8 +45,16 @@ window basic_window(std::string const & title, size_t width, size_t height)
             0); // no flags: basic window
 
     if (!window_ptr) {
-        throw std::runtime_error{SDL_GetError()};
+        std::stringstream message;
+        message << "Window couldn't be created! SDL_Error: "
+                << SDL_GetError();
+        throw std::runtime_error{message.str()};
     }
+    return window_ptr;
+}
+
+SDL_Renderer * load_renderer(SDL_Window * window_ptr)
+{
 
     SDL_Renderer * renderer = SDL_CreateRenderer(
             window_ptr,
@@ -49,32 +62,24 @@ window basic_window(std::string const & title, size_t width, size_t height)
             SDL_RENDERER_ACCELERATED);
 
     if (!renderer) {
-        SDL_DestroyWindow(window_ptr);
-        throw std::runtime_error{SDL_GetError()};
+        std::stringstream message;
+        message << "Window renderer couldn't be created! SDL_Error: "
+                << SDL_GetError();
+        SDL_DestroyWindow(window_ptr); // clean up
+        throw std::runtime_error{message.str()};
     }
-    return window(window_ptr, renderer);
+    return renderer;
+}
+
+window basic_window(std::string const & title, size_t width, size_t height)
+{
+    auto window_ptr = load_window(title, width, height);
+    return window(window_ptr, load_renderer(window_ptr));
 }
 
 window basic_blit_window(std::string const & title, size_t width, size_t height)
 {
-    // initialize video if it hasn't already been done
-    if (!SDL_WasInit(SDL_INIT_VIDEO) && SDL_InitSubSystem(SDL_INIT_VIDEO)) {
-        throw std::runtime_error{SDL_GetError()};
-    }
-
-    // create the window and a renderer for it
-    SDL_Window * window_ptr = SDL_CreateWindow(
-            title.c_str(),
-            // use default ("undefined") window position
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            width, height,
-            SDL_WINDOW_SHOWN); // make sure the window renders
-
-    if (!window_ptr) {
-        throw std::runtime_error{SDL_GetError()};
-    }
-
-    return window(window_ptr, nullptr);
+    return window(load_window(title, width, height), nullptr);
 }
 
 }
