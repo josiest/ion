6cf4a2a61186e425d9fd31f47c5a34ed58fb3967
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-20 01:47:01 -0700

    separated main game into header and source

---
+++ examples/muncher/include/muncher.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "entities/player.hpp"
+#include "entities/munchable.hpp"
+
+#include <ion/ion.hpp>
+#include <SDL2/SDL_events.h>
+#include <entt/entity/registry.hpp>
+
+#include <string>
+#include <cstdint>
+#include <random>
+
+/**
+ * A manager class for game resources
+ */
+class muncher {
+public:
+    // delete any unwanted implicit constructors
+    muncher() = delete;
+    muncher(muncher const &) = delete;
+
+    /**
+     * The type of the random engine used
+     */
+    using engine_t = std::mt19937;
+
+    /**
+     * Create a muncher game
+     *
+     * \param width the width of the game window
+     * \param height the height of the game window
+     */
+    muncher(std::uint32_t width, std::uint32_t height) noexcept;
+
+    /**
+     * Run the game
+     */
+    void run() noexcept;
+
+    /**
+     * Reset the game
+     */
+    void reset() noexcept;
+
+    /**
+     * Determine if the game is in a valid state to run
+     */
+    inline bool is_ok() const noexcept
+    {
+        return _error.empty();
+    }
+
+    /**
+     * Get the error message explaining why the game can't run
+     */
+    inline std::string error() const noexcept
+    {
+        return _error;
+    }
+
+    /**
+     * Set the error message that explains why the game can't run
+     */
+    inline void error(std::string const & message) noexcept
+    {
+        _error = message;
+    }
+private:
+    // sdl resources
+
+    // it's important to put the sdl context before any other sdl resources
+    ion::sdl_context _sdl;
+    ion::render_window _window;
+
+    // events and input
+    ion::event_system _events;
+    ion::input::keyboard_axis _input;
+
+    // entt setup
+    engine_t _rng;
+    entt::registry _entities;
+
+    // entity references and managers
+    entt::entity _player;
+
+    munchable_factory _munchables;
+    double _munchtime_likelihood;
+
+    // error handling
+    std::string _error;
+
+    entt::entity _make_player() noexcept;
+};
+
+muncher & get_game() noexcept;
+void reset_game(SDL_Event const & event);
--- examples/muncher/src/muncher.cpp
+++ examples/muncher/src/muncher.cpp
@@ -1,3 +1,5 @@
+#include "muncher.hpp"
+
 #include "entities/player.hpp"
 #include "entities/munchable.hpp"
 
@@ -8,116 +10,106 @@
 #include <entt/entity/registry.hpp>
 #include <SDL2/SDL.h>
 
+#include <iostream>
 #include <string>
+#include <cstdint>
 #include <random>
 
-void reset_game(SDL_Event const & event);
+muncher & get_game() noexcept
+{
+    static muncher game{800, 600};
+    return game;
+}
 
-// a class that manages sdl resources
-class muncher {
-public:
-    using engine_t = std::mt19937;
+int main()
+{
+    auto & game = get_game();
 
-    muncher(size_t width, size_t height)
-
-          // intialize the muncher game object with the specified width and height
-        : _title{"Muncher"}, _width(width), _height(height),
-
-          // create a wasd keyboard input axis
-          _input(_events, SDLK_d, SDLK_a, SDLK_w, SDLK_s),
-
-          // initialize the random engine to a random seed
-          _rng{std::random_device{}()}, 
-
-          // create the player entity and the munchable factory
-          _player(_make_player()), _munchables{_width, _height, _rng}
-    {
-        _events.subscribe(SDL_QUIT, &ion::input::quit_on_event);
-        _events.subscribe(SDL_KEYUP, &reset_game);
+    // crash if the game failed to initialize properly
+    if (not game.is_ok()) {
+        std::cout << game.error() << std::endl;
     }
+    // otherwise run the game
+    else {
+        game.run();
+    }
+}
 
-    // make sure that sdl quits once the game goes out of scope
-    ~muncher() { SDL_Quit(); }
+muncher::muncher(std::uint32_t width, std::uint32_t height) noexcept
 
-    void run()
-    {
-        // declare the sdl context before any other sdl resources
-        ion::sdl_context sdl;
+      // intialize the window with specified dimensions
+    : _window{"Muncher", width, height},
 
-        // create the window, specifying title and dimensions
-        auto window = ion::render_window(_title, _width, _height);
+      // create a wasd keyboard input axis
+      _input(_events, SDLK_d, SDLK_a, SDLK_w, SDLK_s),
 
-        // timer for physics
-        ion::clock clock;
+      // initialize the random engine with a pseudo-random seed
+      _rng{std::random_device{}()}, 
 
-        // decider for making a munchable
-        std::binomial_distribution<bool> is_munch_time(1, .01);
+      // create the player entity and the munchable factory
+      _player{_make_player()},
+      _munchables{width, height, _rng}, _munchtime_likelihood{.01}
+{
+    // quit when the user exits the window
+    _events.subscribe(SDL_QUIT, &ion::input::quit_on_event);
 
-        // run the program
-        while (!ion::input::has_quit()) {
-            // handle events
-            _events.process_queue();
+    // reset the game when the user presses the reset key
+    _events.subscribe(SDL_KEYUP, &reset_game);
+}
 
-            // apply systems
+void muncher::run() noexcept
+{
+    // timer for physics
+    ion::clock clock;
 
-            // decide if this is the frame to make a new munchable
-            if (is_munch_time(_rng)) {
-                _munchables.make_munchable(_entities, _player);
-            }
-            // physics systems
-            float const dt = clock.tick();
-            accelerate_player(_entities, _player, _input, dt);
-            move_munchies(_entities, dt);
+    // decider for making a munchable
+    std::binomial_distribution<bool> is_munch_time(1, _munchtime_likelihood);
 
-            // mechanic systems
-            devour(_entities, _player);
-            _munchables.filter(_entities);
+    // run the program
+    while (!ion::input::has_quit()) {
+        // handle events
+        _events.process_queue();
 
-            // render
-            render(window, _entities);
+        // apply systems
+
+        // decide if this is the frame to make a new munchable
+        if (is_munch_time(_rng)) {
+            _munchables.make_munchable(_entities, _player);
         }
+        // physics systems
+        float const dt = clock.tick();
+        accelerate_player(_entities, _player, _input, dt);
+        move_munchies(_entities, dt);
+
+        // mechanic systems
+        devour(_entities, _player);
+        _munchables.filter(_entities);
+
+        // render
+        render(_window, _entities);
     }
-    void reset()
-    {
-        if (_entities.valid(_player)) {
-            _entities.destroy(_player);
-        }
-        _player = _make_player();
+}
+
+void muncher::reset() noexcept
+{
+    if (_entities.valid(_player)) {
+        _entities.destroy(_player);
     }
-private:
-    std::string const _title;
-    size_t _width;
-    size_t _height;
+    _player = _make_player();
+}
 
-    ion::event_system _events;
-    ion::input::keyboard_axis _input;
-
-    engine_t _rng;
-    entt::registry _entities;
-
-    entt::entity _player;
-    munchable_factory _munchables;
-
-    entt::entity _make_player()
-    {
-        return make_player(_entities, static_cast<int>(_width)/2,
-                                      static_cast<int>(_height)/2, 15);
-    }
-};
-
-muncher GAME{800, 600};
 void reset_game(SDL_Event const & event)
 {
     // do nothing if not the right event or not the right key
     if (event.type != SDL_KEYUP || event.key.keysym.sym != SDLK_r) {
         return;
     }
-    GAME.reset();
+    get_game().reset();
 }
 
-int main()
+entt::entity muncher::_make_player() noexcept
 {
-    // run the muncher game then quit sdl
-    GAME.run();
+    int width, height;
+    SDL_GetWindowSize(_window.sdl_window(), &width, &height);
+    return make_player(_entities, width/2, height/2, 15);
 }
-
