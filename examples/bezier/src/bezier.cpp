92828ee713336f791e643bd48dfdca763500d2f6
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-24 09:27:40 -0700

    started bezier example with new result-style types

--- examples/bezier/src/bezier.cpp
+++ examples/bezier/src/bezier.cpp
@@ -1,5 +1,4 @@
 #include "bezier.hpp"
-#include "serialize/shader.hpp"
 
 #include <ion/ion.hpp>
 #include <SDL.h>
@@ -22,17 +21,18 @@ int main()
     bezier example{800, 600};
 
     // crash if initialization failed
-    if (example.bad()) {
-        std::cerr << example.error();
+    if (not example) {
+        std::cerr << example.error() << std::endl;
         return 1;
     }
+    // otherwise run the program
     example.run();
 }
 
 void bezier::run() noexcept
 {
     // busy loop until the user quits
-    while (!ion::input::has_quit()) {
+    while (not ion::input::has_quit()) {
         _events.process_queue();
     }
 }
@@ -43,55 +43,43 @@ bezier::bezier(std::uint32_t width, std::uint32_t height) noexcept
 {
     _events.subscribe(SDL_QUIT, &ion::input::quit_on_event);
     // failure if sdl resources failed to initialize
-    if (_sdl.bad()) {
+    if (not _sdl) {
         _error = "SDL couldn't initialize: "s + _sdl.error();
         return;
     }
-    if (_window.bad()) {
+    if (not _window) {
         _error = "Window couldn't initialize: "s + _window.error();
         return;
     }
 
-    // intialize glew
-    glewExperimental = GL_TRUE;
-    GLenum const glew_error = glewInit();
-
-    // failure if flew couldn't initialize
-    if (glew_error != GLEW_OK) {
-        // glew erorr strings are unsigned chars - reinterpret to c-string
-        _error = "Glew couldn't initialize: "s +
-            reinterpret_cast<const char *>(glewGetErrorString(glew_error));
-        return;
-    }
-
     // try to load the shader sources into filestreams
-    auto bezier_sources = serialize::load_shader_sources("bezier", "../shaders");
+    //auto bezier_sources = serialize::load_shader_sources("bezier", "../shaders");
 
-    // if any of the shader sources fail to load print out which ones
-    if (ranges::any_of(bezier_sources, serialize::load_failed)) {
+    //// if any of the shader sources fail to load print out which ones
+    //if (ranges::any_of(bezier_sources, serialize::load_failed)) {
 
-        // initialize the error with a helpful message
-        std::ostringstream message;
-        message << "The following shader files failed to load:" << std::endl;
+    //    // initialize the error with a helpful message
+    //    std::ostringstream message;
+    //    message << "The following shader files failed to load:" << std::endl;
 
-        // put the error message for each file on a new line
-        auto messages = bezier_sources | views::filter(serialize::load_failed)
-                                       | views::transform(serialize::load_error);
-        ranges::copy(messages, std::ostream_iterator<std::string>(message, "\n"));
-        _error = message.str();
-        return;
-    }
+    //    // put the error message for each file on a new line
+    //    auto messages = bezier_sources | views::filter(serialize::load_failed)
+    //                                   | views::transform(serialize::load_error);
+    //    ranges::copy(messages, std::ostream_iterator<std::string>(message, "\n"));
+    //    _error = message.str();
+    //    return;
+    //}
 
-    // convert the load-info to a source map
-    ion::shader_program::source_map bezier_shader;
-    auto into_shader = std::inserter(bezier_shader, bezier_shader.end());
-    ranges::transform(bezier_sources, into_shader, serialize::load_source);
+    //// convert the load-info to a source map
+    //ion::shader_program::source_map bezier_shader;
+    //auto into_shader = std::inserter(bezier_shader, bezier_shader.end());
+    //ranges::transform(bezier_sources, into_shader, serialize::load_source);
 
-    // failure if the shader sources fail to compile
-    _shaders.push_back(std::make_unique<ion::shader_program>(
-                       std::move(bezier_shader))
-    );
-    if (_shaders.back()->bad()) {
-        _error = _shaders.back()->error();
-    }
+    //// failure if the shader sources fail to compile
+    //_shaders.push_back(std::make_unique<ion::shader_program>(
+    //                   std::move(bezier_shader))
+    //);
+    //if (_shaders.back()->bad()) {
+    //    _error = _shaders.back()->error();
+    //}
 }
--- examples/bezier/src/bezier.hpp
+++ examples/bezier/src/bezier.hpp
@@ -1,33 +1,23 @@
 #pragma once
 
-#include "serialize/shader.hpp"
-
-#include <GL/glew.h>
-#include <GL/glu.h>
-
 #include <ion/ion.hpp>
 #include <ion/gl.hpp>
 
-#include <unordered_map>
-#include <vector>
-
-#include <memory>
 #include <cstdint>
+#include <string>
 
 class bezier {
 public:
     bezier(std::uint32_t width, std::uint32_t height) noexcept;
 
     void run() noexcept;
-    inline bool good() const noexcept { return _error.empty(); }
-    inline bool bad() const noexcept { return not _error.empty(); }
-    inline std::string error() const noexcept { return _error; }
+
+    inline bool operator!() const noexcept { return not _error.empty(); }
+    inline std::string const & error() const noexcept { return _error; }
 private:
     ion::sdl_context _sdl;
     ion::glwindow _window;
 
     ion::event_system _events;
-
-    std::vector<std::unique_ptr<ion::shader_program>> _shaders;
     std::string _error;
 };
