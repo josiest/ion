161a4454161257f665fc26c1e4cc9746b6de7366
Josie Thompson <dot.slash.josie@gmail.com>
2021-09-27 12:53:38 -0700

    made path constructor for surface explicit

--- include/ion/graphics/surface.hpp
+++ include/ion/graphics/surface.hpp
@@ -24,7 +24,7 @@ public:
      * Create a surface by loading a bitmap image
      * \param path the path of the bitmap image
      */
-    surface(std::filesystem::path const & path) noexcept;
+    explicit surface(std::filesystem::path const & path) noexcept;
 
     /**
      * Create a surface from another temporary surface
