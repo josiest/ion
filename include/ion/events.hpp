07a71d45277fcafe02151ce58c3a4686d3944edc
Josie Thompson <dot.slash.josie@gmail.com>
2022-01-23 23:07:58 -0800

    minor revisions to coding style in events

--- include/ion/events.hpp
+++ include/ion/events.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
-#include <SDL_events.h>
+// frameworks
+#include <SDL.h>
+
+// datastructures
 #include <unordered_map>
 #include <vector>
-#include <functional>
+
+// data types
+#include <functional> // std::function
+#include <cstdint> // std::uint32_t
 
 namespace ion {
 
 typedef void (*listener_fp)(SDL_Event const &);
-using listener_fxn = std::function<void(SDL_Event const &)>;
+using listener_fn = std::function<void(SDL_Event const &)>;
 
 class event_system {
 public:
@@ -21,7 +27,7 @@ public:
      * \note This should be the preferred method for subscribing functions
      *       because it has the least overhead
      */
-    void subscribe(uint32_t event_type, listener_fp callback);
+    void subscribe(std::uint32_t event_type, listener_fp callback);
 
     /**
      * Subscribe an arbitrary function listener
@@ -32,14 +38,14 @@ public:
      * \note Be cautious when using this method because std::function has a lot
      *       of overhead and it may unnecesarily slow down your game
      */
-    void subscribe_functor(uint32_t event_type, listener_fxn callback);
+    void subscribe_functor(std::uint32_t event_type, listener_fn callback);
 
     /**
      * Poll all SDL events in the queue, calling any listeners along the way.
      */
     void process_queue();
 private:
-    std::unordered_map<uint32_t, std::vector<listener_fp>> _listeners_by_type;
-    std::unordered_map<uint32_t, std::vector<listener_fxn>> _listener_functors;
+    std::unordered_map<std::uint32_t, std::vector<listener_fp>> _listeners_by_type;
+    std::unordered_map<std::uint32_t, std::vector<listener_fn>> _listener_functors;
 };
 }
--- src/events.cpp
+++ src/events.cpp
@@ -1,10 +1,23 @@
 #include "ion/events.hpp"
+
+// frameworks
 #include <SDL.h>
+
+// data types
+#include <cstdint> // std::uint32_t
+
+// data structures
+#include <vector>
+
+// algorithms
 #include <algorithm>
+
+// aliases
+using uint = std::uint32_t;
 
 namespace ion {
 
-void event_system::subscribe(Uint32 event_type, listener_fp callback)
+void event_system::subscribe(uint event_type, listener_fp callback)
 {
     // default the event listeners to an empty vector
     std::vector<listener_fp> listeners;
@@ -17,13 +30,13 @@ void event_system::subscribe(Uint32 event_type, listener_fp callback)
 
     // add the current listener and update the listener map
     listeners.push_back(callback);
-    _listeners_by_type[event_type] = listeners;
+    _listeners_by_type.insert_or_assign(event_type, listeners);
 }
 
-void event_system::subscribe_functor(Uint32 event_type, listener_fxn callback)
+void event_system::subscribe_functor(uint event_type, listener_fn callback)
 {
     // default the event listeners to an empty vector
-    std::vector<listener_fxn> listeners;
+    std::vector<listener_fn> listeners;
 
     // but get them if they exist
     auto search = _listener_functors.find(event_type);
@@ -33,7 +46,7 @@ void event_system::subscribe_functor(Uint32 event_type, listener_fxn callback)
 
     // add the current listener and update the listener map
     listeners.push_back(callback);
-    _listener_functors[event_type] = listeners;
+    _listener_functors.insert_or_assign(event_type, listeners);
 
 }
 
@@ -42,16 +55,18 @@ void event_system::process_queue()
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
 
+        // TODO: iterate callbacks in *true* order subscribed (interleaved)
+
         // run each subscribed callback fp with the event if any exist
         auto fp_search = _listeners_by_type.find(event.type);
         if (fp_search != _listeners_by_type.end()) {
             auto listeners = fp_search->second;
             for (auto callback : listeners) { callback(event); }
         }
-        // run each subscribed callback fxn with the event if any exist
-        auto fxn_search = _listener_functors.find(event.type);
-        if (fxn_search != _listener_functors.end()) {
-            auto listeners = fxn_search->second;
+        // run each subscribed callback fn with the event if any exist
+        auto fn_search = _listener_functors.find(event.type);
+        if (fn_search != _listener_functors.end()) {
+            auto listeners = fn_search->second;
             for (auto callback : listeners) {
                 callback(event);
             }
